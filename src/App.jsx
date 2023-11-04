a4e5a7aa4e548e31e9fcb98e8a3260e3083e65df
rejaul rasel <rejaulhasanrasel28@gmail.com>
2023-11-04 11:06:50 +0600

    added users get api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
 
+  const [users, setUsers] = useState([]);
 
+  useEffect( () => {
+    fetch('http://localhost:4000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[users])
+
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const address = form.address.value;
+    const user = { name, address }
+
+
+
+    fetch('http://localhost:4000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if(data.insertedId){
+          alert('user added')
+          form.reset();
+        }
+      })
+
+
+  }
   return (
     <>
-     <h1>Hello world hello!!</h1>
-     <h1>heelo prithibi@@@</h1>
+      <h1>USER MANAGEMENT</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="text" name="address" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <h1>{users.length}</h1>
     </>
   )
 }
